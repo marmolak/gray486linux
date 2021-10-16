5a39bb6da24fd00e73b5857ddcfe0e8df7ddea1e
Robin Hack <hack.robin@gmail.com>
2021-10-16 18:22:32 +0200

    Add missing config && fix typos in run.sh

--- releases/hanggai/src/gray486/test/run.sh
+++ releases/hanggai/src/gray486/test/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-qemu-system-i386 -cpu 486 -m 24 --kernel arch/x86/boot/bzImage -nographic -append "consol=tty0" -serial mon:stdio
+qemu-system-i386 -cpu 486 -m 24 --kernel arch/x86/boot/bzImage -nographic -append "console=ttyS0" -serial mon:stdio
