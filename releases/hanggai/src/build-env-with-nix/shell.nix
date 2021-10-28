4e30009ed42a7f94d25f48980dec5bd82ddba1a7
Robin Hack <hack.robin@gmail.com>
2021-10-28 20:40:58 +0200

    build: Add nix-shell environment.
    Add nix-shell script for build environment.
    gcc needs rebuild because derivation disables CET.

---
+++ releases/hanggai/src/build-env-with-nix/shell.nix
@@ -0,0 +1,30 @@
+with import <nixpkgs> {};
+with pkgsi686Linux;
+
+let
+    gccNoCet = gcc11.cc.overrideAttrs (oldAttrs : rec { 
+        configureFlags = [ "--disable-cet" ] ++ oldAttrs.configureFlags;
+    });
+ 
+    gccNoCetWrap = wrapCCWith rec {
+        cc = gccNoCet;
+    };   
+
+in (overrideCC stdenv gccNoCetWrap).mkDerivation
+{
+    name = "gccnocet";
+    hardeningDisable = [ "all" ];
+
+    buildInputs = [
+        ncurses
+        ncurses.dev
+	gnumake
+	flex
+	bison
+    ];
+
+    shellHook = ''
+	export CFLAGS="-m32 -march=i486 -mtune=i486 -fcf-protection=none -fno-stack-protector -fomit-frame-pointer -mno-mmx -mno-sse -fno-pic"
+	export CC="gcc"
+    '';
+}
