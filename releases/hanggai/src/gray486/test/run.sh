d3a99c94a449e06e5b0f08802d05a61b105fc6cc
Robin Hack <hack.robin@gmail.com>
2021-11-18 19:18:13 +0100

    kernel/config: Enable mutexes and epoll. Enable parport.
    This add some nice functionality but it adds some resources
    so this must be reflected in run.sh in test dir.

--- releases/hanggai/src/gray486/test/run.sh
+++ releases/hanggai/src/gray486/test/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #DEBUG="-s -S"
-MEM="13m"
+MEM="14m"
 
 qemu-system-i386 -M microvm $DEBUG -cpu 486 -m "${MEM}" --kernel arch/x86/boot/bzImage -nographic -append "console=ttyS0" -serial mon:stdio
