adc6f32b93a3b257ffc61dc3fc9147902427ded0
Robin Hack <hack.robin@gmail.com>
2024-09-19 12:33:45 +0200

    kernel: update to 6.11.

--- releases/hanggai/src/lib/kernel.sh
+++ releases/hanggai/src/lib/kernel.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-declare -r KERNEL_VERSION="linux-6.10"
+declare -r KERNEL_VERSION="linux-6.11"
--- releases/hanggai/src/linux-6.10/drivers/firewire/core-trace.c
+++
@@ -1,5 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-// Copyright (c) 2024 Takashi Sakamoto
-
-#define CREATE_TRACE_POINTS
-#include <trace/events/firewire.h>
